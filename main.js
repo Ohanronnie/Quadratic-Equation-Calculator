7884e088fa11ab7dcb67cb53facb4fe16df2bd90
Ohanronnie <96521591+Ohanronnie@users.noreply.github.com>
2022-06-03 16:32:25 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function myfunc() {
+  a = document.getElementById('num1').value;
+  b = document.getElementById('num2').value;
+  c = document.getElementById('num3').value;
+  ans0 = -b;
+
+  ans00 = b * b;
+
+  ans01 = ans00 - 4 * a * c;
+
+  ans10 = -b + Math.sqrt(ans01);
+
+  ans1 = ans10 / 2 * a;
+
+  ans20 = -b - Math.sqrt(ans01);
+
+  ans2 = ans20 / 2 * a;
+
+  document.getElementById('ans').innerHTML = 'x' + ' ' + '=' + '  ' + ans1 + ' ' + 'or' + ' ' + 'y' + ' ' + '=' + ' ' + ans2;
+  if (isNaN(ans2) == true) {
+    alert('The question is not correct')
+    document.getElementById('ans').innerHTML = 'x' + ' ' + '=' + '  ' + 0 + ' ' + 'or' + ' ' + 'y' + ' ' + '=' + ' ' + 0;
+
+  }
+}
